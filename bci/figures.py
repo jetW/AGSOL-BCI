807250a1cee3fcb8454bbba2f7222d17a725779b
Mingye <mingye@brandeis>
2014-05-28 03:04:53 -0400

    Customized multiple colored bar chart implemented.

--- bci/figures.py
+++ bci/figures.py
@@ -1,21 +1,24 @@
 
 import pylab
 
-def customized_bar_chart(coordinates, values, texts, barwidth, **kwargs):
+def customized_bar_chart(coordinates, values, barwidth, **kwargs):
 	if not isinstance(values[0], list):
 		values = [values]
+	colors = kwargs.get('colors', [None] * len(coordinates))
+	texts = kwargs.get('texts', [''] * len(coordinates))
 
 	ax = pylab.axes()
 	for i in range(len(coordinates)):
 
 		# Bars
 		x, y = coordinates[i]
+		color = colors[i]
 		basewidth = barwidth * (0.5 + 1.5 * len(values))
 		baseleft = x - basewidth * 0.5
 		for j in range(len(values)):
 			value = values[j][i]
 			barleft = baseleft + barwidth * (0.5 + 1.5 * j)
-			ax.broken_barh([(barleft, barwidth)], (y, value))
+			ax.broken_barh([(barleft, barwidth)], (y, value), facecolors=color)
 		ax.broken_barh([(baseleft, basewidth)], (y, 0))
 
 		# Texts
