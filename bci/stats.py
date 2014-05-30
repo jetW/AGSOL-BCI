6c3d9797553200a8eee9045e40f14d2591308cfa
Mingye <mingye@brandeis>
2014-05-30 19:04:52 -0400

    Code for drawing texts refactored.

--- bci/stats.py
+++ bci/stats.py
@@ -2,7 +2,10 @@
 def map(x, old_min, old_max, new_min, new_max):
 	old_range = old_max - old_min
 	new_range = new_max - new_min
-	return float(x - old_min) / old_range * new_range + new_min
+	if old_range != 0:
+		return float(x - old_min) / old_range * new_range + new_min
+	else:
+		return float(new_min + new_max) / 2
 
 def rescale(array, new_min, new_max):
 	old_max = max(array)
