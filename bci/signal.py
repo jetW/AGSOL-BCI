95f052a3182645a57257481416173fb757551611
Mingye <mingye@brandeis>
2014-06-16 15:04:23 -0400

    Laplacian spatial filter implemented.

--- bci/signal.py
+++ bci/signal.py
@@ -1,5 +1,5 @@
 
-import scipy.signal
+import scipy.signal, numpy
 
 def butter_bandpass_filter(signal, frequency, band, order):
 	# http://en.wikipedia.org/wiki/Nyquist-Shannon_sampling_theorem
@@ -8,3 +8,14 @@ def butter_bandpass_filter(signal, frequency, band, order):
 	Wn = [lower_cutoff / nyquist, upper_cutoff / nyquist]
 	b, a = scipy.signal.butter(order, Wn, btype='bandpass')
 	return scipy.signal.filtfilt(b, a, signal)
+
+def laplacian_filter(signal_matrix, laplacian_matrix):
+	# In the signal matrix:
+	# Each row is a sampling point; each column is a channel
+	# In the Laplacian matrix:
+	# Each row is an output channel; each column is an input channel
+	filtered_signal_matrix = []
+	for signal in signal_matrix:
+		filtered_signal = numpy.inner(signal, laplacian_matrix)
+		filtered_signal_matrix.append(filtered_signal)
+	return filtered_signal_matrix
