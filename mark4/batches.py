8fc41975489772c7c80551d4400269eebbfaa779
Mingye <mingye@brandeis>
2014-12-12 13:01:55 -0500

    Simplified batch functions.
    Merged wrappers.py and batches.py.
    Separated m.data transpose and window configuration.
    Hard coded variable names in .mat files.
    Removed most verbose printing.
    Refactored most functions.

--- mark4/batches.py
+++ mark4/batches.py
@@ -1,91 +1,70 @@
 
 import numpy, scipy.io
 import bci.signal, bci.features
-import wrappers
-import weka.arff
+import weka.arff, weka.evaluate, weka.classify
 
-def batch_bandpass_filtering(sources, destination_lists, bands, channels,
-	frequency, mTot='mTot', transpose=False, verbose=0):
+def _loadmat(path):
+	return scipy.io.loadmat(path, struct_as_record=False, squeeze_me=True)
+
+def batch_mdata_transpose(sources, destinations):
+	for i, source in enumerate(sources):
+		mTot = _loadmat(source)['mTot']
+		for j, m in enumerate(mTot):
+			m.data = numpy.transpose(m.data)
+		scipy.io.savemat(destinations[i], {'mTot': mTot}, oned_as='row')
+
+def batch_bandpass_filtering(sources, destination_lists,
+	bands, channels, frequency, verbose=0):
 
 	for i, source in enumerate(sources):
 		if verbose >= 1:
 			print '[%d/%d] Filtering %s' % (i+1, len(sources), source)
 
-		vmTot = wrappers.load_mat_variable(source, mTot)
-
+		mTot = _loadmat(source)['mTot']
 		for j, band in enumerate(bands):
 			if verbose >= 2:
 				print '[%d/%d] For band %s' % (j+1, len(bands), band)
 
-			for k, m in enumerate(vmTot):
+			for k, m in enumerate(mTot):
 				if verbose >= 3:
-					print '[%d/%d] At movement %s' % (k+1, len(vmTot), m.trial)
-
-				if transpose:
-					matrix, mx = numpy.transpose(m.data), []
-				else:
-					matrix, mx = m.data, []
+					print '[%d/%d] At movement %s' % (k+1, len(mTot), m.trial)
 
 				for channel in channels:
-					mx.append(
-						bci.signal.butter_bandpass_filter(
-							matrix[channel-1], frequency, band, 5))
+					m.data[channel-1] = bci.signal.butter_bandpass_filter(
+						m.data[channel-1], frequency, band, 5)
 
-				if transpose:
-					m.data = numpy.transpose(mx)
-				else:
-					m.data = mx
+			scipy.io.savemat(destination_lists[i][j], {'mTot': mTot}, oned_as='row')
 
-			scipy.io.savemat(destination_lists[i][j], {mTot: vmTot}, oned_as='row')
-
-def batch_extracting_latencies(sources, chair, epoch,
-	transpose=False, mTot='mTot', verbose=False):
+def batch_extracting_latencies(sources, epoch, chair):
+	threshold = chair['threshold'] * chair['constant']
+	channel = chair['channel']
 
 	latency_lists = []
-
 	for i, source in enumerate(sources):
-		if verbose:
-			print '[%d/%d] Extracting %s' % (i+1, len(sources), source)
-
-		vmTot = wrappers.load_mat_variable(source, mTot)
-		channel = chair['channel']
-		threshold = chair['threshold'] * chair['constant']
+		mTot = _loadmat(source)['mTot']
 
 		latencies = []
-
-		for j, m in enumerate(vmTot):
-
-			if transpose:
-				m.data = numpy.transpose(m.data)
-
+		for j, m in enumerate(mTot):
 			latency = bci.features.extract_latency(
 				m.data[channel-1], m.time, threshold, epoch)
 			latencies.append(latency)
 
 		latency_lists.append(latencies)
-
 	return latency_lists
 
-def batch_extracting_difference_latencies(sources, eye, epoch, coefficients,
-	transpose=False, mTot='mTot', verbose=False):
+def batch_extracting_difference_latencies(sources, ksources, epoch, eye):
+	channel, channel1, channel2 = eye['channel'], eye['channel1'], eye['channel2']
 
 	latency_lists = []
-
 	for i, source in enumerate(sources):
-		if verbose:
-			print '[%d/%d] Extracting %s' % (i+1, len(sources), source)
+		ksource = ksources[i]
 
-		vmTot = wrappers.load_mat_variable(source, mTot)
-		channel, channel1, channel2 = eye['channel'], eye['channel1'], eye['channel2']
-		threshold = eye['threshold'] * numpy.abs(coefficients[i])
+		mTot = _loadmat(source)['mTot']
+		cal_k = _loadmat(ksource)['cal_k']
+		threshold = eye['threshold'] * numpy.abs(cal_k)
 
 		latencies = []
-
-		for j, m in enumerate(vmTot):
-
-			if transpose:
-				m.data = numpy.transpose(m.data)
-
+		for j, m in enumerate(mTot):
 			m.data[channel-1] = m.data[channel1-1] - m.data[channel2-1]
 			m.data[channel-1] = bci.signal.butter_lowpass_filter(
 				m.data[channel-1], epoch['frequency'], 5, 5)
@@ -94,27 +73,17 @@ def batch_extracting_difference_latencies(sources, eye, epoch, coefficients,
 			latencies.append(latency)
 
 		latency_lists.append(latencies)
-
 	return latency_lists
 
-def batch_extracting_features(sources, destinations,
-	channels, configurations, epoch,
-	latency_lists=None, start_offset=0, stop_offset=0,
-	do_mean=False, do_power=False, transpose=False,
-	mTot='mTot', featmat='featmat', mclass='mclass',
-	verbose=False):
+def batch_configure_windows(configurations, epoch, nsources, nmovements,
+	latency_lists=None, start_offset=0, stop_offset=0):
 
-	for i, source in enumerate(sources):
-		destination = destinations[i]
+	windows_lists = []
+	for i in range(nsources):
 
-		if verbose:
-			print '[%d/%d] Extracting %s' % (i+1, len(sources), source)
+		windows_list = []
+		for j in range(nmovements):
 
-		vmTot = wrappers.load_mat_variable(source, mTot)
-
-		vfeatmat = []
-
-		for j, m in enumerate(vmTot):
 			windows = []
 			if latency_lists == None:
 				for c in configurations:
@@ -125,88 +94,90 @@ def batch_extracting_features(sources, destinations,
 					c.set_relative_mode(latency, start_offset, stop_offset)
 					windows.extend(c.get_windows(epoch))
 
-			if transpose:
-				matrix, mx = numpy.transpose(m.data), []
-			else:
-				matrix, mx = m.data, []
+			windows_list.append(windows)
 
-			for channel in channels:
-				if do_mean:
-					mx.extend(
-						bci.features.extract_means(
-							matrix[channel-1], windows))
-				if do_power:
-					mx.extend(
-						bci.features.extract_powers(
-							matrix[channel-1], windows))
+		windows_lists.append(windows_list)
+	return windows_lists
 
-			vfeatmat.append(mx)
-
-		vmclass = [m.type for m in vmTot]
-
-		scipy.io.savemat(destination, {featmat: vfeatmat, mclass: vmclass}, oned_as='row')
-
-def batch_combining(source_lists, destinations, variable, axis,
-	passing_variables=[], verbose=False):
-
-	for i, sources in enumerate(source_lists):
-		destination = destinations[i]
-
-		if verbose:
-			print '[%d/%d] Combining to %s' % (
-				i+1, len(source_lists), destinations[i])
-
-		wrappers.combine_mat_matrices(
-			sources, destination, variable, axis, passing_variables)
-
-def batch_converting(sources, destinations, relations,
-	featmat='featmat', mclass='mclass', verbose=False):
+def batch_extracting_features(sources, destinations,
+	channels, windows_lists, do_mean=False, do_power=False):
 
 	for i, source in enumerate(sources):
 		destination = destinations[i]
 
-		if verbose:
-			print '[%d/%d] Converting %s' % (i+1, len(sources), source)
+		mTot = _loadmat(source)['mTot']
 
-		vfeatmat = wrappers.load_mat_variable(source, featmat)
-		vmclass = wrappers.load_mat_variable(source, mclass)
+		featmat = []
+		for j, m in enumerate(mTot):
+			windows = windows_lists[i][j]
+
+			row = []
+			if do_mean:
+				for channel in channels:
+					row.extend(bci.features.extract_means(m.data[channel-1], windows))
+			if do_power:
+				for channel in channels:
+					row.extend(bci.features.extract_powers(m.data[channel-1], windows))
+
+			featmat.append(row)
+
+		mclass = [m.type for m in mTot]
+
+		scipy.io.savemat(destination, {'featmat': featmat, 'mclass': mclass}, oned_as='row')
+
+def batch_combining(source_lists, destinations):
+	for i, sources in enumerate(source_lists):
+		destination = destinations[i]
+
+		mat = _loadmat(sources[0])
+		featmat = mat['featmat']
+		mclass = mat['mclass']
+
+		for source in sources[1:]:
+			featmat2 = _loadmat(source)['featmat']
+			featmat = numpy.concatenate((featmat, featmat2), axis=1)
+
+		dictionary = {'featmat': featmat, 'mclass': mclass}
+		scipy.io.savemat(destination, dictionary, oned_as='row')
+
+def batch_converting(sources, destinations, relations):
+	for i, source in enumerate(sources):
+		destination = destinations[i]
+
+		mat = _loadmat(source)
+		featmat = mat['featmat']
+		mclass = mat['mclass']
 
 		relation = relations[i]
 		attributes = [('BCI_%d' % (j+1), 'numeric')
-			for j in range(len(vfeatmat[0]))]
-		data = vfeatmat
+			for j in range(len(featmat[0]))]
+		data = featmat
 		classes = ('class', '{4,5}')
-		classifications = vmclass
+		classifications = mclass
 
 		weka.arff.write_arff(destination, relation, attributes, data,
 			classes, classifications)
 
-def batch_infogain_evaluating(paths, classpath, threshold=0, verbose=False):
-
+def batch_infogain_evaluating(paths, classpath, threshold=0):
 	evaluation_lists = []
-
 	for i, path in enumerate(paths):
-		if verbose:
-			print '[%d/%d] Evaluating %s' % (i+1, len(paths), path)
-
-		evaluation_lists.append(
-			wrappers.evaluate_with_infogain(classpath, path, threshold))
-
+		output = weka.evaluate.evaluate_attribute(classpath,
+			'InfoGainAttributeEval', 'Ranker -T %f' % threshold, path)
+		evaluations = weka.evaluate.extract_evaluations(output)
+		evaluation_lists.append(evaluations)
 	return evaluation_lists
 
-def batch_infogain_svm_classifying(trainings, tests, classpath,
-	threshold=0, verbose=False):
-
+def batch_infogain_svm_classifying(trainings, tests, classpath, threshold=0):
 	accuracies = []
-
 	for i, training in enumerate(trainings):
 		test = tests[i]
-
-		if verbose:
-			print '[%d/%d] Classifying %s' % (i+1, len(trainings), training)
-
-		accuracies.append(
-			wrappers.classify_with_infogain_svm(
-				classpath, training, test, threshold))
-
+		output = weka.classify.classify_data(classpath,
+			'weka.classifiers.meta.FilteredClassifier ' +
+			'-F "weka.filters.supervised.attribute.AttributeSelection ' +
+			'-E \\"weka.attributeSelection.InfoGainAttributeEval\\" ' +
+			'-S \\"weka.attributeSelection.Ranker -T %f\\"" ' % threshold +
+			'-W weka.classifiers.functions.SMO',
+			training, '-T "' + test + '"')
+		accuracy = weka.classify.extract_test_accuracy(output)
+		accuracies.append(accuracy)
 	return accuracies
